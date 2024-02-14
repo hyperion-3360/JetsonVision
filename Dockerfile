da3150ec3ddae2a9cdf7bfb31621e89138da8754
Nikola Zelovic <nikola1998@hotmail.fr>
2024-02-14 00:13:08 -0500

    Record video with the robot's jetson

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,6 @@ FROM nvcr.io/nvidia/l4t-pytorch:r35.2.1-pth2.0-py3
 WORKDIR app
 
 COPY ./src ./
+COPY ./pipdeps ./pipdeps
 
-RUN python3 -m pip install -r requirements.txt
+RUN python3 -m pip install --no-index --find-links /app/pipdeps/ -r requirements.txt
--- src/record-clips.py
+++ src/record-clips.py
@@ -7,19 +7,17 @@ import cv2
 
 QUIT = False
 
-def record(cameraid: int):
-  store = Path('/home/data/clip')
-
-  store += len(os.listdir(store)) # folder is named by the take number. E.g. .....clip/0/ or .....clip/1/, etc
-  store.mkdir(parents=True, exist_ok=True)
-
+def record(cameraid: int, store: Path):
   cap = cv2.VideoCapture(cameraid)
   if not cap.isOpened():
     print(f"Failed to init camera {cameraid}")
     return
 
-  out = cv2.VideoWriter(store / f"clip-{cameraid}.mp4", cv2.VideoWriter_fourcc('m', 'p', '4', 'v'), 30, (1280, 720))
 
+  print(store / f"clip-{cameraid}.mp4")
+  out = cv2.VideoWriter(str(store / f"clip-{cameraid}.mp4"), cv2.VideoWriter_fourcc('m', 'p', '4', 'v'), 30, (1280, 720))
+
+  global QUIT
   while not QUIT: # meh we don't care about thread safety here. We're just reading and we assume only 1 thread will change the QUIT value. In the worst case, we will stop a couple of frames later
     r, f = cap.read()
     if(r):
@@ -29,15 +27,25 @@ def record(cameraid: int):
   cap.release()
 
 if __name__ == "__main__":
-  def stop():
-    QUIT = False  # We should use a lock, but whatever.... It's probably atomic and the other threads using QUIT should only read its value
+  def stop(sig, frame):
+    global QUIT
+    QUIT = True  # We should use a lock, but whatever.... It's probably atomic and the other threads using QUIT should only read its value
+
   signal.signal(signal.SIGINT, stop)
 
-  cameras = [0, 1]  # camera ids
+  # Get the storage location
+  store = Path('./test/data/clip') # For Docker container, set this path to the mounted volume (/home/data)
+  store.mkdir(parents=True, exist_ok=True)
+
+  store = store / f"{1 + len(os.listdir(store))}" # folder is named by the take number. E.g. .....clip/0/ or .....clip/1/, etc.
+  store.mkdir()
+
+  # Start the recording
+  cameras = [0, 2]  # camera ids
 
   threads = []
   for camera in cameras:
-    t = Thread(target = record, args=[camera])
+    t = Thread(target = record, args=[camera, store])
     threads.append(t)
 
     t.start()
